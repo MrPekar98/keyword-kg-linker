998f9ad91b8922e7e7ab73af858373eedb55e6d0
MrPekar98 <martin.pekar@outlook.dk>
2023-11-16 11:00:48 +0100

    Pretty print

--- src/main/java/dk/aau/dkwe/Main.java
+++ src/main/java/dk/aau/dkwe/Main.java
@@ -116,8 +116,12 @@ public class Main
             }
 
             documents.add(new Document(entity, label, description, neighborDescription));
-            System.out.print("\t\t\t\t\r");
-            System.out.print("Progress: " + (((double) ++progress / entityCount) * 100) + "%\r");
+
+            if (progress % 100 == 0)
+            {
+                System.out.print("\t\t\t\t\r");
+                System.out.print("Progress: " + (((double) ++progress / entityCount) * 100) + "%\r");
+            }
         }
 
         return documents;
